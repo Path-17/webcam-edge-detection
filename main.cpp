5a11e98ac8c944010de4d5f2c7c9ddba7b362c0c
Georgy Gesse <gessegeorgy@gmail.com>
2021-06-07 10:09:23 -0400

    sobel + prewitt edge detection, swap with argv1

--- main.cpp
+++ main.cpp
@@ -3,34 +3,75 @@
 #include<opencv4/opencv2/imgproc/imgproc.hpp>
 #include<opencv4/opencv2/highgui/highgui.hpp>
 
+
+enum options{
+    sobel, prewitt
+};
+
 // Computes the x component of the gradient vector
 // at a given point in a image.
 // returns gradient in the x direction
-int xGradient(cv::Mat image, int x, int y)
+int xGradient(cv::Mat image, int x, int y, int option)
 {
-    return image.at<uchar>(y-1, x-1) +
+    if(option == prewitt){
+        return image.at<uchar>(y-1, x-1) +
+                image.at<uchar>(y, x-1) +
+                 image.at<uchar>(y+1, x-1) -
+                  image.at<uchar>(y-1, x+1) -
+                   image.at<uchar>(y, x+1) -
+                    image.at<uchar>(y+1, x+1);
+    }
+    if(option == sobel){
+        return image.at<uchar>(y-1, x-1) +
                 2*image.at<uchar>(y, x-1) +
                  image.at<uchar>(y+1, x-1) -
                   image.at<uchar>(y-1, x+1) -
                    2*image.at<uchar>(y, x+1) -
                     image.at<uchar>(y+1, x+1);
+    }
 }
  
 // Computes the y component of the gradient vector
 // at a given point in a image
 // returns gradient in the y direction
  
-int yGradient(cv::Mat image, int x, int y)
+int yGradient(cv::Mat image, int x, int y, int option)
 {
-    return image.at<uchar>(y-1, x-1) +
+    if(option == prewitt){
+        return image.at<uchar>(y-1, x-1) +
+                image.at<uchar>(y-1, x) +
+                 image.at<uchar>(y-1, x+1) -
+                  image.at<uchar>(y+1, x-1) -
+                   image.at<uchar>(y+1, x) -
+                    image.at<uchar>(y+1, x+1);
+    }
+    if(option == sobel){
+        return image.at<uchar>(y-1, x-1) +
                 2*image.at<uchar>(y-1, x) +
                  image.at<uchar>(y-1, x+1) -
                   image.at<uchar>(y+1, x-1) -
                    2*image.at<uchar>(y+1, x) -
                     image.at<uchar>(y+1, x+1);
+    }
+    
 }
 
 int main(int argc, char** argv){
+
+    std::string name;
+    int option;
+
+    if(argc == 2){
+        if(argv[1] == "prewitt"){
+            name = "Prewitt";
+            option = prewitt;
+        }
+        else if(argv[1] == "sobel"){
+            name = "Sobel";
+            option = sobel;
+        }
+
+    }
 
     cv::Mat frame ,dst;
     
@@ -58,8 +99,8 @@ int main(int argc, char** argv){
  
         for(int y = 1; y < frame.rows - 1; y++){
             for(int x = 1; x < frame.cols - 1; x++){
-                gx = xGradient(frame, x, y);
-                gy = yGradient(frame, x, y);
+                gx = xGradient(frame, x, y, option);
+                gy = yGradient(frame, x, y, option);
                 sum = abs(gx) + abs(gy);
                 sum = sum > 255 ? 255:sum;
                 sum = sum < 0 ? 0 : sum;
@@ -67,7 +108,7 @@ int main(int argc, char** argv){
             }
         }
 
-       cv::imshow("Webcam", dst);
+       cv::imshow(name +" edge detection", dst);
        cv::waitKey(33);
     }
 
